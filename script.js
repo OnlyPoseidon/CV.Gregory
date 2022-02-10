d5e5586e5d389c1060eaec054af6ccd898e76614
OnlyPoseidon <MaSaMuNe43>
2022-02-10 16:30:41 +0100

    ajouts debut d'animation + JS

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function openServices(servicesName) {
     }
     document.getElementById(servicesName).style.display = "block";
   }
+
+var moveForce = 30; // max popup movement in pixels
+var rotateForce = 20; // max popup rotation in deg
+
+document.mousemove(function(e) { console.log ("phillipe")
+    var docX = $(document).width();
+    var docY = $(document).height();
+    
+    var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
+    var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
+    
+    var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
+    var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
+    
+    $('.popup')
+        .css('left', moveX+'px')
+        .css('top', moveY+'px')
+        .css('transform', 'rotateX('+rotateX+'deg) rotateY('+rotateY+'deg)');
+});
