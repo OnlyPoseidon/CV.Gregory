84b41e765ff7dd5055ca3d04ea097790df028487
OnlyPoseidon <MaSaMuNe43>
2022-02-11 09:29:56 +0100

    ajouts scrip js et modification css pour logo animé

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-console.log("log")
-
 function openServices(servicesName) {
-    var i;
-    var x = document.getElementsByClassName("services");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(servicesName).style.display = "block";
+  var i;
+  var x = document.getElementsByClassName("services");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  document.getElementById(servicesName).style.display = "block";
+}
 
-var moveForce = 30; // max popup movement in pixels
-var rotateForce = 20; // max popup rotation in deg
+var moveForce = 20; // max popup movement in pixels
+var rotateForce = 10; // max popup rotation in deg
 
-document.mousemove(function(e) { console.log ("phillipe")
-    var docX = $(document).width();
-    var docY = $(document).height();
-    
-    var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
-    var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
-    
-    var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
-    var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
-    
-    $('.popup')
-        .css('left', moveX+'px')
-        .css('top', moveY+'px')
-        .css('transform', 'rotateX('+rotateX+'deg) rotateY('+rotateY+'deg)');
+document.addEventListener("mousemove", function (e) {
+  var docX = document.body.clientWidth;
+  var docY = document.body.clientHeight;
+
+  var moveX = (e.pageX - docX / 2) / (docX / 2) * -moveForce;
+  var moveY = (e.pageY - docY / 2) / (docY / 2) * -moveForce;
+
+  var rotateY = (e.pageX / docX * rotateForce * 2) - rotateForce;
+  var rotateX = -((e.pageY / docY * rotateForce * 2) - rotateForce);
+
+  document.getElementById('popup').style.left = moveX + 'px';
+  document.getElementById('popup').style.top = moveY + 'px';
+  document.getElementById('popup').style.transform = 'rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)';
 });
